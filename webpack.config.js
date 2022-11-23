b778233625948019283090993d233b02fa9a3319
Haris Skiadas <cskiadas@gmail.com>
2022-11-23 02:00:20 -0500

    Add postcss support

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,90 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+module.exports = (env) => ({
     mode: 'development',
     context: path.join(__dirname, 'app'),
-    entry: ['./js/app.ts'],
+    entry: {
+        app: './js/app.ts',
+        styles: './css/main.pcss'
+    },
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: '[name].[contenthash:8].js',
+        publicPath: './',
+        pathinfo: false
     },
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 use: ['ts-loader']
+            },
+            {
+                test: /\.p?(css)$/i,
+                oneOf: [
+                    {
+                        resourceQuery: /module/,
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    url: false,
+                                    sourceMap: env.production !== true,
+                                    importLoaders: 1,
+                                    modules: {
+                                        localIdentName: '[local]_[hash:base64:5]'
+                                    }
+                                }
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: env.production !== true
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        use: [
+                            MiniCssExtractPlugin.loader,
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    url: false,
+                                    sourceMap: env.production !== true,
+                                    importLoaders: 1,
+                                    modules: false
+                                }
+                            },
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: env.production !== true
+                                }
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: env.production === true ? '[name].[contenthash:8].css' : '[name].css',
+            chunkFilename: env.production === true ? '[name].[contenthash:8].css' : '[name].css'
+        }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'app/index.html'),
             inject: 'body'
         })
     ]
-};
+});
