89975a08494c19ca6d7da632ce302884f7d3cb92
Haris Skiadas <cskiadas@gmail.com>
2022-11-23 01:14:40 -0500

    Apply prettier's formatting

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     context: path.join(__dirname, 'app'),
-    entry: [
-        './js/app.ts',
-        ],
+    entry: ['./js/app.ts'],
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 exclude: /node_modules/,
-                use: [
-                    'ts-loader',
-                    ],
-            },
-            ],
+                use: ['ts-loader']
+            }
+        ]
     },
-    plugins: [new HtmlWebpackPlugin({
-       'template': path.resolve(__dirname, 'app/index.html'),
-       'inject': 'body'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'app/index.html'),
+            inject: 'body'
+        })
+    ]
 };
